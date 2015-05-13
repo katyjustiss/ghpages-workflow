61b3dc26aba924ce7b1960042e3cb7f66bfab5a2
Katy Justiss <katy.justiss@gmail.com>
2015-05-13 13:54:15 -0500

    copy other files during build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # clean and prepare public directory
 rm -rf public
-mkdir public
+cp -r src public
 
 # compile jade to html
 ./node_modules/.bin/jade src -P
@@ -10,15 +10,25 @@ cd src
 find . -name "*.html" | cpio -pdvm ../public
 cd ..
 rm -rf src/*.html \
-  src/**/*.html \
-  public/**/_*.html \
-  public/_partials
+       src/**/*.html \
+       public/**/_*.html \
+       public/_partials
 
 # compile sass to css
 ./node_modules/.bin/node-sass \
   --output-style compressed \
   --source-map-embed \
   src/_styles/main.scss public/css/main.css
+
+# convert ES6 JS to ES5
+ ./node_modules/.bin/babel src --out-dir public -s inline
+
+# clean unneeded files
+rm -rf public/_styles \
+       public/*.jade \
+       public/**/*.jade
+       public/*.scss \
+       public/**/*.scss
 
 
 
